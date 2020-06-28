1448255a8f3d566e24603e425198f0ceb35159f9
pfg <omegariisthebest@gmail.com>
2020-06-28 15:11:01 -0700

    Create stdlib.js

---
+++ stdlib.js
@@ -0,0 +1,13 @@
+window.el = (nme) => document.createElement(nme);
+window.txt = (txt) => document.createTextNode(txt);
+window.anychange = (itms, cb) => (itms.forEach(itm => itm.oninput = () => cb()), cb());
+window.body = document.getElementById("maincontent") || document.body;
+Node.prototype.attr = function(atrs) {Object.entries(atrs).forEach(([k, v]) => this.setAttribute(k, v)); return this;};
+Node.prototype.adto = function(prnt) {prnt.appendChild(this); return this;};
+Node.prototype.adch = function(chld) {this.appendChild(chld); return this;};
+Node.prototype.atxt = function(txta) {this.appendChild(txt(txta)); return this;};
+Node.prototype.onev = function(evnm, cb) {this.addEventListener(evnm, cb); return this;};
+Node.prototype.drmv = function(defer) {defer(() => this.remove()); return this;};
+Node.prototype.clss = function(clss) {clss.split(".").filter(q => q).map(itm => this.classList.add(itm)); return this;};
+Object.prototype.dwth = function(cb) {cb(this); return this;};
+Object.defineProperty(Array.prototype, "last", {get: function() {return this[this.length - 1]}});
