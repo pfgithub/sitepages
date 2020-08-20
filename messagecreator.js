9a67bf3652b532987a2e508faea9aa9778b9eaf0
pfg <omegariisthebest@gmail.com>
2020-08-19 22:55:48 -0700

    add license text

--- messagecreator.js
+++ messagecreator.js
@@ -59,6 +59,23 @@ list.forEach((cb) => cb());
 return res;
 };
 
+/// μhtml
+/// 
+/// ISC License
+///
+/// Copyright (c) 2020, Andrea Giammarchi, @WebReflection
+///
+/// Permission to use, copy, modify, and/or distribute this software for any
+/// purpose with or without fee is hereby granted, provided that the above
+/// copyright notice and this permission notice appear in all copies.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+/// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+/// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+/// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+/// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+/// OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+/// PERFORMANCE OF THIS SOFTWARE.
 window.uhtml=function(e){"use strict";function t(n){return{get:function(e){return n.get(e)},set:function(e,t){return n.
 set(e,t),t}}}function u(e,t,n){return i.test(t)?e:"<".concat(t).concat(n.replace(p,""),"></").concat(t,">")}function f(
 t,a,e){for(var n=[],r=t.length,o=function(r){var e=t[r-1];n.push(l.test(e)&&function e(t,n){return 0<n--&&(s.test(t[n])
